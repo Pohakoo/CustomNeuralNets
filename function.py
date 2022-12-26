89584beb2effb66fdaee810d078959bd1286eebc
Pohakoo <73570104+Pohakoo@users.noreply.github.com>
2022-12-25 19:47:10 -1000

    Gross, icky, and bad, yet functional

--- function.py
+++ function.py
@@ -11,7 +11,6 @@ import imageio
 #It's fine if the file names are different
 
 def train(dataFolder, outputFolder, labelsIndex, epochs, inputType='wav', hiddenLayers=[], sameLength=True, generate=False, possibleLabels=0, device='GPU:0', npyoutputfolder=None, npyfilesfolder=None, optimizer="adam"):
-    print(epochs)
     if hiddenLayers == []:
         if not generate:
             hiddenLayers = [128, 64, 64]
@@ -32,30 +31,40 @@ def train(dataFolder, outputFolder, labelsIndex, epochs, inputType='wav', hidden
         data = []
         labels = []
 
+        i = 0
         # Iterate over the index data
         for item in index_data:
             # Load the audio data from the .WAV file
-            print(item)
-            audio, sample_rate = sf.read(dataFolder + '/' + item)
+            print("loading " + item)
+            audio, sample_rate = sf.read(dataFolder + '/' + str(item))
 
             # Reshape the numpy array
-            audio = np.reshape(audio, (audio.shape[0]*2))
-            print(np.shape(audio))
+            audio = audio.flatten()
 
             # Add the audio data and label to the lists
             data.append(audio)
             if generate == False:
                 labels.append(index_data[item])
             else:
-                audio, sample_rate = sf.read(dataFolder + '/' + index_data[item])
-                audio = np.reshape(audio, (audio.shape[0] * 2))
-                labels.append(audio)
+                out_audio, sample_rate = sf.read(dataFolder + '/' + str(index_data[item]))
+                out_audio = out_audio.flatten()
+                if i==0:
+                    generationSize = np.shape(out_audio)[0]
+                if np.shape(out_audio)[0] == generationSize:
+                    labels.append(out_audio)
+                else:
+                    del data[-1]
+            i += 1
+
 
         # Convert the lists to NumPy arrays
         data = np.array(data)
         labels = np.array(labels)
 
-        return data, labels
+        if generate:
+            return data, labels, generationSize
+        else:
+            return data, labels, possibleLabels
 
     def img_to_npy(dataFolder, labelsIndex, generate):
         # Load the index data from the json file
@@ -66,40 +75,53 @@ def train(dataFolder, outputFolder, labelsIndex, epochs, inputType='wav', hidden
         data = []
         labels = []
 
+        i = 0
         # Iterate over the index data
         for item in index_data:
+            print("loading " + item)
             # Load the image data from the .WAV file
-            print(item)
-            image = imageio.imread(dataFolder + '/' + item)
-
-            # Flatten the numpy array
-            image = image.flatten()
-
-            # Add the image data and label to the lists
-            data.append(image)
+            image = imageio.imread(dataFolder + '/' + str(item))
+            flat_image = image.flatten()
+            data.append(flat_image)
             if not generate:
-                labels.append(index_data[item])
+                labels.append(int(index_data[item]))
             else:
-                image = imageio.imread(dataFolder + '/' + index_data[item])
-                image = image.flatten()
-                labels.append(image)
+                label_image = imageio.imread(dataFolder + '/' + index_data[item])
+                flat_label_image = label_image.flatten()
+                if i == 0:
+                    generationSize = np.shape(flat_label_image)[0]
+                if np.shape(flat_label_image)[0] == generationSize:
+                    labels.append(flat_label_image)
+                else:
+                    del data[-1]
+            i += 1
 
-        # Convert the lists to NumPy arrays
+        print("formatting files...")
+        if generate:
+            data = [item.flatten() for item in data]
+            labels = [item.flatten() for item in labels]
+
+        print(np.shape(data))
+        print(np.shape(labels))
+
         data = np.array(data)
         labels = np.array(labels)
 
-        return data, labels
+        if generate:
+            return data, labels, generationSize
+        else:
+            return data, labels, possibleLabels
 
     def data_to_npy(inputType, dataFolder, labelsIndex, generate):
         if inputType.lower() == "wav":
-            data, labels = wav_to_npy(dataFolder, labelsIndex, generate)
+            data, labels, generationSize = wav_to_npy(dataFolder, labelsIndex, generate)
         if inputType.lower() == "png" or inputType.lower() == "jpg" or inputType.lower() == "jpeg" or inputType.lower() == "bmp" or inputType.lower() == "tiff" or inputType.lower() == "gif" or inputType.lower() == "pdf" or inputType.lower() == "svg":
-            data, labels = img_to_npy(dataFolder, labelsIndex, generate)
+            data, labels, generationSize = img_to_npy(dataFolder, labelsIndex, generate)
 
-        return data, labels
+        return data, labels, generationSize
 
     if npyfilesfolder == None:
-        data, labels = data_to_npy(inputType=inputType, dataFolder=dataFolder, labelsIndex=labelsIndex, generate=generate)
+        data, labels, generationSize = data_to_npy(inputType=inputType, dataFolder=dataFolder, labelsIndex=labelsIndex, generate=generate)
     else:
         data = np.load(npyfilesfolder + "/data.npy")
         labels = np.load(npyfilesfolder + "/labels.npy")
@@ -109,16 +131,20 @@ def train(dataFolder, outputFolder, labelsIndex, epochs, inputType='wav', hidden
         np.save(npyoutputfolder + '/labels.npy', labels)
 
     def pad_or_truncate_to_average(audio_data, sameLength):
-        # Calculate the target length of the audio clips
+        print(np.shape(audio_data))
+        # Calculate the target length of the data
         if sameLength:
-            target_length = np.mean([data.shape[0] for data in audio_data])
+            if generate:
+                target_length = generationSize
+            else:
+                target_length = np.mean([data.shape[0] for data in audio_data])
         else:
             target_length = np.max([data.shape[0] for data in audio_data])
 
         # Round the target length to the nearest integer
         target_length = int(round(target_length))
 
-        # Get the shape of the audio data
+        # Get the shape of the data
         shape = audio_data.shape
 
         # Create an empty array with the same shape as the audio data
@@ -135,19 +161,43 @@ def train(dataFolder, outputFolder, labelsIndex, epochs, inputType='wav', hidden
             # Leave the data unchanged if it is the right length
             else:
                 padded_data[i] = data
+        if generate:
+            padded_data = padded_data.reshape((padded_data.flatten().shape[0], 1))
 
         return padded_data, target_length
 
-    # Preprocess the audio data
-    preprocessed_data, inputLength = pad_or_truncate_to_average(data, sameLength)
-    if possibleLabels == 0:
+    # Preprocess the data
+    if np.shape(data)[0] * np.shape(labels)[0] > 2147483646 or np.shape(labels)[0] * np.shape(labels)[0] < 1:
+        print("too much input data! Remove some inputs or make the inputs smaller and try again.")
+        exit()
+    if np.shape(labels)[0] * np.shape(labels)[0] > 2147483646 or np.shape(labels)[0] * np.shape(labels)[0] < 1:
+        print("too much output data! Remove some outputs or make the outputs smaller and try again.")
+        exit()
+    print("preprocessing data...")
+    if not generate:
+        preprocessed_data, inputLength = pad_or_truncate_to_average(data, sameLength)
+        labels = labels.reshape((labels.shape[0], 1))
+    else:
+        preprocessed_data = data.flatten().reshape(np.shape(data.flatten())[0], 1)
+        labels = labels.reshape((labels.shape[1]*labels.shape[0], 1))
+
+    if possibleLabels < 2:
         if not generate:
-            possibleLabels = 10
+            print("invalid possibleLabels value")
+            exit()
         else:
-            possibleLabels = inputLength
+            possibleLabels = generationSize
+
+    print('input shape: ' + str(np.shape(preprocessed_data)))
+    print('output shape: ' + str(np.shape(labels)))
 
     # Split the data into training and test sets
-    x_train, x_test, y_train, y_test = train_test_split(preprocessed_data, labels)
+    x_train, x_test, y_train, y_test = train_test_split(preprocessed_data, labels, train_size=0.8)
+
+    print(np.shape(x_train))
+    print(np.shape(y_train))
+    print(np.shape(x_test))
+    print(np.shape(y_test))
 
     # Define the model architecture
     with tf.device('/device:GPU:'+str(device)):
@@ -163,6 +213,7 @@ def train(dataFolder, outputFolder, labelsIndex, epochs, inputType='wav', hidden
     # Train the model and store the training history
     history = model.fit(x_train, y_train, epochs=epochs, validation_data=(x_test, y_test))
 
+    # Save the trained model to a file
     model.save(outputFolder + '/trained_model.h5')
 
     # Extract the accuracy and loss values from the history object
@@ -187,5 +238,3 @@ def train(dataFolder, outputFolder, labelsIndex, epochs, inputType='wav', hidden
     plt.legend(loc='upper right')
     plt.title('Training and Validation Loss')
     plt.show()
-
-    # Save the trained model to a file
